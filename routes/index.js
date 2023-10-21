779e4497ec35eaa2a5095d01a65d978631ee8339
Arjun K K <commonmanaj@gmail.com>
2023-10-21 11:14:55 +0530

    git ignore added

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const router=express.Router()
 router.use(bodyParser.json())
 
 router.get("/",foodController.getAllFoodItems)
+router.get("/check",(req,res)=>{res.json({message:"its working welllll........."})})
 router.post("/addfood",foodController.addReceipe) //sdfsdf
 
 export default router    
